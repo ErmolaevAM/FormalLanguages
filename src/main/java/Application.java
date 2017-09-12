2e57e3f8d5454198178fd08585ca657fd14a99b3
ErmolaevAM <chepucha@yandex.ru>
2017-09-12 22:54:54 +0400

    task1

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,12 +1,13 @@
-import taskOne.DeterministicAutomaton;
+import taskOne.UniversalDeterministicAutomaton;
 
 /**
  * Created by Александр on 06.09.2017.
  */
 public class Application {
     public static void main(String[] args) {
-        DeterministicAutomaton deterministicAutomaton =
-                new DeterministicAutomaton("D:\\ermolaxe\\IdeaProjects\\homedev.testproject\\FormalLanguages\\src\\main\\resources\\taskOne\\data.json");
-        System.out.println(deterministicAutomaton);
+        UniversalDeterministicAutomaton automaton =
+                new UniversalDeterministicAutomaton("D:\\ermolaxe\\GitProject\\FormalLanguages\\src\\main\\resources\\taskOne\\data.json");
+
+        automaton.start("ababab");
     }
 }
