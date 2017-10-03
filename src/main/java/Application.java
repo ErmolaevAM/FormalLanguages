dec744e6a5cb2fd7fbb77bd83f26573dcf81126e
unknown <ErmolaevAM@w12-314-58.main.sgu.ru>
2017-10-03 12:39:26 +0300

    fix undeterministic automa

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -6,13 +6,16 @@ import taskOne.UniversalAutoma;
 public class Application {
     public static void main(String[] args) {
 
-        String deterministicAutomaFile = "D:\\ermolaxe\\GitProject\\FormalLanguages\\src\\main\\resources\\taskOne\\deterministicAutoma.json";
-        String undeterministicAutomaFile = "D:\\ermolaxe\\GitProject\\FormalLanguages\\src\\main\\resources\\taskOne\\undeterministicAutoma.json";
-        String taskTwo = "D:\\ermolaxe\\GitProject\\FormalLanguages\\src\\main\\resources\\taskOne\\taskTwoFile.json";
-        String taskThree = "";
+        String deterministicAutomaFile = "C:\\Users\\ErmolaevAM\\Downloads\\FormalLanguages\\src\\main\\resources\\taskOne\\deterministicAutoma.json";
+        String undeterministicAutomaFile = "C:\\Users\\ErmolaevAM\\Downloads\\FormalLanguages\\src\\main\\resources\\taskOne\\undeterministicAutoma.json";
+        String taskTwo = "C:\\Users\\ErmolaevAM\\Downloads\\FormalLanguages\\src\\main\\resources\\taskTwo\\taskTwoFile.json";
 
-        UniversalAutoma automa = new UniversalAutoma(taskTwo);
-        System.out.println(automa.start("+10.12e7"));
+        UniversalAutoma automa = new UniversalAutoma(undeterministicAutomaFile);
+//        System.out.println(automa.start("ababbaaa")); //15-0, 15-15, 30-15, 30-30, 30-40, 40-40, a>, A
+        System.out.println(automa.start("ks"));
+
+
+//        System.out.println(automa.start("+10.12e7"));
 
 
     }
